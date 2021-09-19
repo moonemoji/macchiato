59d8b2db08296ade4474e79a6f044a24f0d92aa9
moonemojis <amsy12345@gmail.com>
2021-09-19 18:53:01 -0500

    added set pin channel command

--- commands/pins/setPin.js
+++ commands/pins/setPin.js
@@ -1,6 +1,6 @@
 const { SlashCommandBuilder } = require("@discordjs/builders");
-const Sequelize = require("sequelize");
-const BOT_DB = "../../bot.db";
+const { Permissions } = require("discord.js");
+const { Pins } = require("../../db/dbObjects.js");
 
 module.exports = {
     data: new SlashCommandBuilder()
@@ -11,7 +11,18 @@ module.exports = {
                 .setDescription("The pin overflow channel.")
                 .setRequired(true)),
     async execute(interaction) {
+        // pin is unavailable in PM
+        if (!interaction.inGuild()) {
+            await interaction.reply("Sorry, this feature is only available in servers!");
+            return;
+        }
+        else if (!interaction.member.permissions.has(Permissions.FLAGS.MANAGE_MESSAGES)) {
+            await interaction.reply("You need the `Manage Messages` permission to use this command!");
+            return;
+        }
+
         const ch = interaction.options.getChannel("channel");
+        Pins.upsert({ guild_id: interaction.guildId, channel_id: ch.id });
 
         await interaction.reply("Done! From now on, if pins are full, I'll send pins in that channel instead.");
     },
--- db/dbObjects.js
+++ db/dbObjects.js
@@ -3,7 +3,7 @@ const Sequelize = require("sequelize");
 const sequelize = new Sequelize("database", "username", "password", {
     dialect: "sqlite",
     logging: false,
-    storage: "./bot.db",
+    storage: "./db/bot.db",
 });
 
 const Pins = require("../models/pins/Pins.js")(sequelize, Sequelize.DataTypes);
